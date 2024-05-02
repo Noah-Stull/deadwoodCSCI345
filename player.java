3b0a6b64cd0d3741c660f2a001b6d2af87cf3842
danielonesock <colthud@wwu.edu>
2024-05-02 16:19:47 -0700

    Updated player.java

--- player.java
+++ player.java
@@ -1,3 +1,29 @@
 public class Player {
-    
+    private PlayerData playerData;
+    private boolean hasMoved;
+
+    public Player(PlayerData playerData) {
+        this.playerData = playerData;
+        this.hasMoved = false;
+    }
+
+    public int act(Role role) {
+
+    }
+
+    public boolean rehearse(int rehearseChips) {
+
+    }
+
+    public boolean move(Location destination) {
+
+    }
+
+    public boolean upgrade(int rank) {
+
+    }
+
+    public void endTurn() {
+        
+    }
 }
