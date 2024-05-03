b30c4158601b39bdfacef29092218f7d8d1b3f2e
Noah-Stull <129891772+Noah-Stull@users.noreply.github.com>
2024-05-02 22:36:54 -0700

    Skeleton card.java

--- card.java
+++ card.java
@@ -1,3 +1,13 @@
-public class Card {
+public class card {
+    public final role[] roles;
+    public final String name;
+    public final String description;
+
+    //all card data is publically read only, so no getters and setters
     
+    public card(role[] roles, String name, String description) {
+        this.roles = roles;
+        this.name = name;
+        this.description = description;
+    }
 }
