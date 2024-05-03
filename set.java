dc508cee3106258a0815ca62e90318e7c0a62eb3
Noah-Stull <129891772+Noah-Stull@users.noreply.github.com>
2024-05-02 22:48:18 -0700

    Update set.java

--- set.java
+++ set.java
@@ -1,3 +1,13 @@
 public class Set {
-    
+    private role[] roles;
+    private card sceneCard;
+    public final Board board;
+    boolean visited;
+
+    public Set(Board b, role[] r) {
+        board = b;
+        roles = r;
+        sceneCard = b.getCard();
+        visited = false;
+    }
 }
