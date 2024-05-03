04dd2f992b11b6199bf7bea3a76e28f43805efeb
Noah-Stull <129891772+Noah-Stull@users.noreply.github.com>
2024-05-02 22:42:07 -0700

    Update role.java

--- role.java
+++ role.java
@@ -1,3 +1,17 @@
 public class role {
-    
+    public final String name;
+    public final int rank;
+    private Player player; 
+    public role(String name, int rank) {
+        this.name = name;
+        this.rank = rank;
+    }
+
+    public boolean isOccupied() {
+        if (player == null) return false;
+        return true;
+    }
+    public void giveRole(Player p) {
+        this.player = p;
+    }
 }
