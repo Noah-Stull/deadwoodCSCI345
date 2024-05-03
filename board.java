d70e0199f1c782e075587b05881343b51787ed4b
Noah-Stull <129891772+Noah-Stull@users.noreply.github.com>
2024-05-02 22:30:59 -0700

    Skeleton board.java

--- board.java
+++ board.java
@@ -1,3 +1,24 @@
-public class Board {
+import java.io.File;
+
+public class board {
+    Location[] location;
+    card[] deck;
+    int wrapCounter;
+
+    //The XML file may be passed to this constructor
+    public board(File f) {
+        deck = new card[36/*make this number of cards */];
+        //parse XML
+    }
+    private void shuffleDeck() {
+        //use either collections shuffle or Random class with swaps
+    }
+    public card getCard() {
+        return deck[0/*some moving index */];
+    }
     
+    //call to check whether wrapCoutner>1
+    public boolean moreScenes() {
+        return true;
+    }
 }
