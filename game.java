d69c30979b6c95625e9f0002bdccfb39f47ebbd3
Noah-Stull <129891772+Noah-Stull@users.noreply.github.com>
2024-05-07 14:43:36 -0700

    Update game.java

--- game.java
+++ game.java
@@ -7,7 +7,7 @@ public class Game {
 
     public Game(int numPlayers) {
 
-        board = new Board(new File("board.xml"), new File("Card.xml"));
+        board = new Board("board.xml", new File("Card.xml"));
         players = new Player[numPlayers];
         if (numPlayers < 5) {
             for(int i = 0; i < numPlayers; i++) {
