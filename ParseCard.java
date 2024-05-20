94246a35f934614a49fbf3abd2b3751d9d6ba189
danielonesock <colthud@wwu.edu>
2024-05-19 18:01:06 -0700

    Update ParseCard.java

--- ParseCard.java
+++ ParseCard.java
@@ -10,53 +10,60 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ParseCard{
+public class ParseCard {
 
-   public Card[] parse(String fileName) {
-      try {
-          File file = new File(fileName);
-          DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-          DocumentBuilder db = dbf.newDocumentBuilder();
-          Document doc = db.parse(file);
+    private Card parseCard(Element e) {
+        String name = e.getAttribute("name");
+        String img = e.getAttribute("img");
+        int budget = Integer.parseInt(e.getAttribute("budget"));
+        Element sceneElement = (Element) e.getElementsByTagName("scene").item(0);
+        String sceneDescription = sceneElement.getTextContent();
 
-          NodeList cardList = doc.getElementsByTagName("card");
+        List<Role> roleList = new ArrayList<>();
+        NodeList partList = e.getElementsByTagName("part");
+        for (int i = 0; i < partList.getLength(); i++) {
+            Node partNode = partList.item(i);
+            if (partNode.getNodeType() == Node.ELEMENT_NODE) {
+                roleList.add(parseRole((Element) partNode));
+            }
+        }
 
-          Card[] tempCards = new Card[cardList.getLength()];
-          int indexC = 0;
-          for (int i = 0; i < cardList.getLength(); i++) {
-              Element cardElement = (Element) cardList.item(i);
-              String name = cardElement.getAttribute("name");
-              String img = cardElement.getAttribute("img");
-              int budget = Integer.parseInt(cardElement.getAttribute("budget"));
-              String scene = cardElement.getElementsByTagName("scene").item(0).getTextContent();
+        Role[] roles = roleList.toArray(new Role[0]);
+        return new Card(roles, name, sceneDescription, budget, img);
+    }
 
-              
+    private Role parseRole(Element e) {
+        String name = e.getAttribute("name");
+        int level = Integer.parseInt(e.getAttribute("level"));
+        String line = e.getElementsByTagName("line").item(0).getTextContent();
+        return new Role(name, level, line, 1);
+    }
 
-              
-              NodeList partList = cardElement.getElementsByTagName("part");
-              Role[] rTemp = new Role[partList.getLength()];
-              int index = 0;
-              for (int j = 0; j < partList.getLength(); j++) {
-                  Element partElement = (Element) partList.item(j);
-                  String partName = partElement.getAttribute("name");
-                  int level = Integer.parseInt(partElement.getAttribute("level"));
-                  String line = partElement.getElementsByTagName("line").item(0).getTextContent();
-                  Role role = new Role(partName, level, line, 1);
-                  rTemp[index] = role;
-                  index++;
-              }
-              Card card = new Card(rTemp, name, img, budget, scene);
-              tempCards[indexC] = card;
-              
-          }
-          return tempCards;
-      } catch (Exception e) {
-          e.printStackTrace();
-          return null;
-      }
-  }
+    public Card[] parse(String fileName) {
+        try {
+            File file = new File(fileName);
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
+            doc.getDocumentElement().normalize();
 
+            NodeList cardList = doc.getElementsByTagName("card");
+            List<Card> cardArrayList = new ArrayList<>();
 
+            for (int i = 0; i < cardList.getLength(); i++) {
+                Node cardNode = cardList.item(i);
+                if (cardNode.getNodeType() == Node.ELEMENT_NODE) {
+                    cardArrayList.add(parseCard((Element) cardNode));
+                }
+            }
 
-}//class
+            return cardArrayList.toArray(new Card[0]);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
