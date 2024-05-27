eee1512d26bf5d01d627827cc5ee8e440b105ddb
Noah-Stull <129891772+Noah-Stull@users.noreply.github.com>
2024-05-26 20:05:01 -0700

    Create View.java

---
+++ View.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+public class View {
+
+    public static void main(String args[]) {
+        JFrame j = new JFrame("board");
+        j.setVisible(true);
+        j.setSize(800,400); 
+
+    }
+
+
+}
